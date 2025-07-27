3fa8a1f8f026753b2a30abc1d5c15fc173901405
Hoàng Lãm <boluclac1301@gmail.com>
2025-07-27 11:16:02 +0700

    Task3 Update PostForm

--- task-3/src/App.js
+++ task-3/src/App.js
@@ -1,12 +1,13 @@
-import {useState} from 'react';
-import Post from './Post';
-import NewPostForm from './NewPostForm';
+import React from 'react';
+import PostForm from './components/PostForm';
+import PostsList from './components/PostsList';
 
 function App() {
   return (
-    <div>
-      <NewPostForm />
-      <Post />
+    <div className="App">
+      <h1>My Blog</h1>
+      <PostForm />
+      <PostsList />
     </div>
   );
 }
--- task-3/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postReducer from './postSlicer';
-
-export const store = configureStore({
-    reducer:{
-        post: postReducer
-    }
-    
-});
